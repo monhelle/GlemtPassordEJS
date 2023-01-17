52fc2d38993446a9f162ef074d6adf3acafeb543
Monica Stefferud-Helle <monicastef@viken.no>
2023-01-17 21:27:15 +0100

    able to retrieve info from input fields

--- public/js/newUser.js
+++ public/js/newUser.js
@@ -1,5 +1,8 @@
 console.log("new user")
 
 function saveNewUser() {
-    
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let repeatPassword = document.getElementById("repeatPassword").value;
+    console.log(email, password, repeatPassword)
 }
