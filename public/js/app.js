09c2cfee95de9146a8469398e581d54ecdde2ab5
Monica Stefferud-Helle <monicastef@viken.no>
2023-01-17 21:19:27 +0100

    adding script tag with static js file and using it to retrieve info from the user at log in account

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,7 @@
-console.log("hello")
+
+function getUserInfo() {
+    let email = document.getElementById("email")
+    let password = document.getElementById("password")
+    console.log(email.value)
+    console.log(password.value)
+}
